6eb9d0740cb8caead5c1ea0c9f822b030690cf8a
farsa <puppetcooder@gmail.com>
2024-05-27 20:28:33 +0300

    branch main/user-svc add grpc server

--- userService/delivery/grpc/grpc_server.go
+++ userService/delivery/grpc/grpc_server.go
@@ -1,1 +1,30 @@
-package grpc
+package delivery
+
+import (
+	"net"
+	"user-service/delivery/grpc/handler"
+	"user-service/internal/interfaces"
+	"user-service/pb"
+
+	// Replace with your package paths
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+type grpcServer struct {
+	userService interfaces.UserService
+	userHandler *handler.UserHandler // Handler for authentication service methods
+}
+
+func NewGRPCServer(userService interfaces.UserService, userHandler *handler.UserHandler) *grpcServer {
+	return &grpcServer{userService: userService, userHandler: userHandler}
+}
+
+func (s *grpcServer) Serve(lis net.Listener) error {
+	grpcServer := grpc.NewServer()
+	pb.RegisterUserServiceServer(grpcServer, s.userHandler) // Register handler for AuthService
+
+	// Enable server reflection
+	reflection.Register(grpcServer)
+	return grpcServer.Serve(lis)
+}
