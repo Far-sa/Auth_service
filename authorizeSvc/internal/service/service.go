b51f36f5cd6db33f6a334bc79603115949132635
farsa <puppetcooder@gmail.com>
2024-06-20 16:48:25 +0300

    branch main/remove unsed copy of main svc

--- authorizeSvc/internal/service/service.go
+++ authorizeSvc/internal/service/service.go
@@ -1,126 +1,137 @@
 package service
 
-// import (
-// 	"authorization-service/infrastructure/messaging"
-// 	"authorization-service/internal/interfaces"
-// 	"context"
-// 	"encoding/json"
-// 	"log"
-// )
+import (
+	"authorization-service/internal/entity"
+	"authorization-service/internal/interfaces"
+	"authorization-service/internal/param"
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+)
 
-// type AuthorizationService struct {
-// 	roleRepository interfaces.RoleRepository
-// 	consumer       *messaging.RabbitMQConsumer
-// 	// publisher      interfaces.EventPublisher
-// }
+const DefaultRole = "User"
 
-// func NewAuthorizationService(roleRepository interfaces.RoleRepository, consumer *messaging.RabbitMQConsumer) *AuthorizationService {
-// 	return &AuthorizationService{roleRepository: roleRepository, consumer: consumer}
-// }
+type AuthzService struct {
+	roleRepo interfaces.RoleRepository
+	consumer interfaces.RoleEvents
+	// publisher      interfaces.EventPublisher
+}
 
-// type Event struct {
-// 	Type EventType // The type of the event (e.g., USER_CREATED, USER_UPDATED)
-// 	User *User     // The user that the event pertains to
-// }
+func NewAuthzService(roleRepo interfaces.RoleRepository, consumer interfaces.RoleEvents) *AuthzService {
+	return &AuthzService{roleRepo: roleRepo, consumer: consumer}
+}
 
-// type EventType int
+func (s *AuthzService) AssignRole(ctx context.Context, req param.RoleAssignmentRequest) error {
+	// Attempt to retrieve the existing role for the user
+	role, err := s.roleRepo.GetRoleByUserID(ctx, req.UserID)
+	if err != nil {
+		if errors.Is(err, interfaces.ErrRoleNotFound) {
+			// If no role is found, assign the default role
+			err = s.roleRepo.AssignRole(ctx, req.UserID, DefaultRole)
+			if err != nil {
+				// Handle error when assigning a new role fails
+				log.Printf("Failed to assign default role to user %s: %v", req.UserID, err)
+				return err
+			}
+			// Log successful default role assignment
+			log.Printf("Assigned default role to user %s", req.UserID)
+		} else {
+			// Log and return any other error that occurred while getting the role
+			log.Printf("Failed to get role for user %s: %v", req.UserID, err)
+			return err
+		}
+	} else {
+		// If the user already has a role, there's nothing to do
+		log.Printf("User %s already has a role assigned: %s", req.UserID, role.Name)
+	}
 
-// // ! events
-// func (s *AuthorizationService) Start() error {
-// 	if err := s.consumer.StartConsuming(); err != nil {
-// 		log.Fatalf("Failed to start consumer: %v", err)
-// 	}
+	// The role assignment was successful, or the user already had a role
+	return nil
+}
 
-// 	// Process messages in a separate goroutine
-// 	go func() {
-// 		for msg := range msgs {
-// 			event := &user.Event{}
-// 			if err := json.Unmarshal(msg.Body, event); err != nil {
-// 				log.Printf("Failed to unmarshal message: %v", err)
-// 				continue
-// 			}
+func (s *AuthzService) UpdateUserRole(ctx context.Context, req param.RoleUpdateRequest) error {
 
-// 			switch event.Type {
-// 			case user.Event_USER_CREATED:
-// 				// Handle user created event
-// 				s.handleUserCreated(event)
-// 			case user.Event_USER_ROLE_UPDATED:
-// 				// Handle user role updated event
-// 				s.handleUserRoleUpdated(event)
-// 			}
-// 		}
-// 	}()
-// 	return nil
-// }
+	// Check if the new role is different from the current one
+	currentRole, err := s.roleRepo.GetRoleByUserID(ctx, req.UserID)
+	if err != nil {
+		// Handle error, possibly not found or db error
+		return fmt.Errorf("error retrieving current role for user %s: %w", req.UserID, err)
+	}
 
-// func (s *AuthorizationService) handleUserCreated(event *user.Event) {
-// 	// Extract user data from event
-// 	newUser := event.User
+	if currentRole.Name == req.NewRole {
+		// No update needed, return early
+		return nil
+	}
 
-// 	// Assign default role to new user
-// 	defaultRole := "user" // replace with your actual default role
-// 	err := s.assignRoleToUser(newUser.Id, defaultRole)
-// 	if err != nil {
-// 		log.Printf("Failed to assign role to new user: %v", err)
-// 		return
-// 	}
+	newRole := entity.Role{Name: req.NewRole}
+	err = s.roleRepo.UpdateRole(ctx, currentRole.ID, newRole)
+	if err != nil {
+		return fmt.Errorf("failed to update role for user %s: %w", req.UserID, err)
+	}
 
-// 	// Update internal state with new user's role
-// 	// ...
-// }
+	log.Printf("Updated role for user %s to %s", req.UserID, newRole.Name)
+	return nil
+}
 
-// func (s *AuthorizationService) handleUserRoleUpdated(event *user.Event) {
-// 	// Update internal state with updated user's role
-// 	// ...
+func (s *AuthzService) ListenForUserEvents() error {
+	// Declare the exchange
+	if err := s.consumer.DeclareExchange("user_events_exchange", "topic"); err != nil {
+		return fmt.Errorf("failed to declare exchange: %w", err)
+	}
 
-// 	// Extract user data from event
-// 	updatedUser := event.User
+	// Create the queue
+	q, err := s.consumer.CreateQueue("user_created_queue", true, false)
+	if err != nil {
+		return fmt.Errorf("failed to create queue: %w", err)
+	}
 
-// 	// Update internal state with updated user's role
-// 	err := s.updateUserRoleInState(updatedUser.Id, updatedUser.Role)
-// 	if err != nil {
-// 		log.Printf("Failed to update user's role: %v", err)
-// 		return
-// 	}
-// }
+	// Create the binding
+	if err := s.consumer.CreateBinding(q.Name, "usr.created.*", "user_events_exchange"); err != nil {
+		return fmt.Errorf("failed to create binding: %w", err)
+	}
 
-// // ! events
-// func (s *AuthorizationService) HandleUserAuthenticatedEvent(ctx context.Context, message string) error {
-// 	// Extract user information from the message
-// 	userID := extractUserIDFromMessage(message)
-// 	log.Printf("Handling UserAuthenticated event for user: %s", userID)
+	// Consume messages from the queue
+	msgs, err := s.consumer.Consume(q.Name, "authorize_svc", false)
+	if err != nil {
+		return fmt.Errorf("failed to consume messages: %w", err)
+	}
 
-// 	// Update user roles/permissions in the repository
-// 	return s.roleRepository.UpdateUserRoles(ctx, userID, "new_role")
-// }
+	//* go s.processMessages(msgs)
+	// Process messages in a separate goroutine
+	go func() {
+		for d := range msgs {
+			var userID string
+			if err := json.Unmarshal(d.Body, &userID); err != nil {
+				log.Printf("Error parsing message: %s", err)
+				continue
+			}
 
-// func (s *AuthorizationService) assignRoleToUser(ctx context.Context, username, role string) error {
-// 	err := s.roleRepository.AssignRole(ctx, username, role)
-// 	if err != nil {
-// 		return err
-// 	}
+			// Assign role to the user
+			if err := s.AssignRole(context.Background(), param.RoleAssignmentRequest{UserID: userID}); err != nil {
+				log.Printf("Error assigning role to user: %s", err)
+				continue
+			}
+		}
+	}()
 
-// 	// event := map[string]string{"username": username, "role": role}
-// 	// eventData, _ := json.Marshal(event)
-// 	// err = s.publisher.Publish("role_assigned", eventData)
-// 	// if err != nil {
-// 	// 	log.Printf("Failed to publish event: %v", err)
-// 	// }
+	log.Printf("Waiting for messages. To exit press CTRL+C")
 
-// 	return nil
-// }
-// func (s *AuthorizationService) updateUserRoleInState(userId, role string) error {
-// 	// Implement the logic to update a user's role in the internal state
-// 	// This might involve updating a database or an in-memory data structure, for example
-// 	// ...
-// 	return nil
-// }
+	// Use a channel to wait for a signal to stop listening for messages
+	stopChan := make(chan struct{})
+	go func() {
+		sigChan := make(chan os.Signal, 1)
+		signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+		<-sigChan
+		close(stopChan)
+	}()
 
-// func (s *AuthorizationService) CheckPermission(ctx context.Context, username, permission string) (bool, error) {
-// 	return s.roleRepository.CheckPermission(ctx, username, permission)
-// }
+	<-stopChan // Wait here until told to stop
+	log.Printf("Shutting down user event listener")
 
-// func extractUserIDFromMessage(message string) string {
-// 	// Simulating extraction logic
-// 	return message
-// }
+	return nil
+}
